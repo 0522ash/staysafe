f0a6d46c957195f773f04e54570614f4c1e1eeea
0522ash <asukaito0522@gmail.com>
2021-04-11 10:10:55 +0900

    news

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,9 @@
 class TopController < ApplicationController
-  def index
-  end
+  require "open-uri"
+    def index
+      api = Rails.env['NEWS_API_KEY']
+      uri = "https://newsapi.org/v2/everything?q=%E3%82%AF%E3%83%AA%E3%82%B9%E3%83%9E%E3%82%B9&sortBy=popularity&apiKey=#{ENV['NEWS_API_KEY']}"
+      article_serialized = open(uri).read
+      @articles = JSON.parse(article_serialized)
+    end
 end
